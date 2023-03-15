cc6145677ba859eda3abf1b88778113db980f243
Jotavesse <jvs2@cin.ufpe.br>
2023-03-15 10:00:34 -0300

    added more methods and operations to vector and point classes

--- RayTracing.cpp
+++ RayTracing.cpp
@@ -2,27 +2,6 @@
 #include <cmath>
 using namespace std;
 
-class Point{
-    public:
-        float x, y, z;
-
-        Point(float xx, float yy, float zz){
-            x = xx;
-            y = yy;
-            z = zz;
-        }
-        Point(float xx){
-            x = xx;
-            y = xx;
-            z = xx;
-        }
-        Point(){
-            x = 0;
-            y = 0;
-            z = 0;
-        }
-};
-
 class Vector{
     public:
         float x, y, z;
@@ -74,12 +53,91 @@ class Vector{
 
             return Vector();
         }
+
+        float dot(Vector vec){
+            return x * vec.x + y * vec.y + z * vec.z; 
+        }
+
+        Vector cross(Vector vec){
+            return Vector(
+            y * vec.z - z * vec.y, 
+            z * vec.x - x * vec.z, 
+            x * vec.y - y * vec.x);
+        }
+
+        const float operator [] (uint8_t i) const { return (&x)[i]; }
+        float& operator [] (uint8_t i) { return (&x)[i]; }
+
+        friend std::ostream& operator << (std::ostream &s, const Vector &v)
+        {
+            return s << '(' << v.x << ' ' << v.y << ' ' << v.z << ')';
+        }
 };
 
-class Camera{
+class Point{
     public:
+        float x, y, z;
 
-        Camera(){
+        Point(float xx, float yy, float zz){
+            x = xx;
+            y = yy;
+            z = zz;
+        }
+        Point(float xx){
+            x = xx;
+            y = xx;
+            z = xx;
+        }
+        Point(){
+            x = 0;
+            y = 0;
+            z = 0;
+        }
+
+        Vector operator + (const Point &v) const
+        {return Vector(x + v.x, y + v.y, z + v.z);}
+
+        Vector operator - (const Point &v) const
+        {return Vector(x - v.x, y - v.y, z - v.z);}
+
+        Vector operator * (const float &n) const
+        {return Vector(x * n, y * n, z * n);}
+
+        const float operator [] (uint8_t i) const { return (&x)[i]; }
+        float& operator [] (uint8_t i) { return (&x)[i]; }
+
+        friend std::ostream& operator << (std::ostream &s, const Point &v)
+        {
+            return s << '(' << v.x << ' ' << v.y << ' ' << v.z << ')';
+        }
+};
+
+
+
+class Camera{
+    
+    public:
+        Point center;
+        Point target;
+        Vector up;
+
+        Vector orthoW;
+        Vector orthoV;
+        Vector orthoU;
+
+        float distScreen;
+        int height;
+        int width;
+
+        Camera(int height, int width, float distScreen, Vector up, Point center, Point target){
+            height = height;
+            width = width;
+            distScreen = distScreen;
+            up = up;
+            center = center;
+            target = target;
+
+            orthoW = (target - center);
             cout << "Criando camera";
         }
 };
@@ -87,6 +145,6 @@ class Camera{
 int main() {
     Vector vec(1,5,3);
     vec.normalized();
-    cout << vec.normalized().x;
+    cout << vec;
     return 0;
 };
