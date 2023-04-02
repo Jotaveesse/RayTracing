d9b50c872e4be479866ff454cfde4e5c633c731d
Icaro-Nunes <icarolimanunes@gmail.com>
2023-04-02 00:19:37 -0300

    Implementação de cópia, inversa e aplicação em vetor de transformação afim

--- Objects.h
+++ Objects.h
@@ -502,6 +502,30 @@ class Mesh: public Object{
         }
 };
 
+void swap(float* row1, float* row2){
+    for(int i = 0; i < 4; i++){
+        float tmp = row1[i];
+        row1[i] = row2[i];
+        row2[i] = tmp;
+    }
+}
+
+void partialPivot(float (*m)[4], float (*inverse)[4], int i) {
+    double maxElement = abs(m[i][i]);
+    int maxRow = i;
+    // Encontra a linha com o maior elemento na coluna i
+    for (int j = i + 1; j < 4; j++) {
+        if (abs(m[j][i]) > maxElement) {
+            maxElement = abs(m[j][i]);
+            maxRow = j;
+        }
+    }
+    // Troca a linha atual com a linha com o maior elemento
+    if (maxRow != i) {
+        swap(m[i], m[maxRow]);
+        swap(inverse[i], inverse[maxRow]);
+    }
+}
 
 class Transform{
     public: 
@@ -513,7 +537,7 @@ class Transform{
                 {0, 0, 0, 1}
             };
 
-        Point apply(Point p){
+        Point apply(Point& p){
             Point result = Point();
             float res[4] = {0, 0, 0, 0};
 
@@ -522,7 +546,24 @@ class Transform{
                     this->matrix[i][0]*p.x +
                     this->matrix[i][1]*p.y +
                     this->matrix[i][2]*p.z +
-                    this->matrix[i][0];
+                    this->matrix[i][0]*1;
+            }
+
+            result.x = res[0];
+            result.y = res[1];
+            result.z = res[2];
+            return result;
+        }
+
+        Vector apply(Vector& v){
+            Vector result;
+            float res[3] = {0, 0, 0};
+
+            for(int i = 0; i < 3; i++){
+                res[i] =
+                    this->matrix[i][0]*v.x +
+                    this->matrix[i][1]*v.y +
+                    this->matrix[i][2]*v.z;
             }
 
             result.x = res[0];
@@ -531,8 +572,60 @@ class Transform{
             return result;
         }
         
+
+        Transform copy(){
+            Transform result;
+
+            for(int i = 0; i < 4; i++){
+                for(int j = 0; j < 4; j++){
+                    result.matrix[i][j] = this->matrix[i][j];
+                }
+            }
+
+            return result;
+        }
+
+        Transform inverse(){
+            Transform result;
+            Transform copy = this->copy();
+
+            float (*m)[4] = copy.matrix;
+            float (*inverse)[4] = result.matrix;
+
+            // Aplica a eliminação gaussiana com pivoteamento parcial
+            for (int i = 0; i < 4; i++) {
+                partialPivot(m, inverse, i);
+                double pivot = m[i][i];
+                // Divide a linha atual pelo pivô
+                for (int j = 0; j < 4; j++) {
+                    m[i][j] /= pivot;
+                    inverse[i][j] /= pivot;
+                }
+                // Subtrai a linha atual das linhas abaixo dela
+                for (int j = i + 1; j < 4; j++) {
+                    double factor = m[j][i];
+                    for (int k = 0; k < 4; k++) {
+                        m[j][k] -= factor * m[i][k];
+                        inverse[j][k] -= factor * inverse[i][k];
+                    }
+                }
+            }
+            // Aplica a eliminação gaussiana com pivoteamento parcial inverso
+            for (int i = 4 - 1; i >= 0; i--) {
+                for (int j = i - 1; j >= 0; j--) {
+                    double factor = m[j][i];
+                    for (int k = 0; k < 4; k++) {
+                        m[j][k] -= factor * m[i][k];
+                        inverse[j][k] -= factor * inverse[i][k];
+                    }
+                }
+            }
+
+            return result;
+        }
+
         Transform operator * (const Transform &other) const{
-            Transform result = Transform();
+            Transform result;
 
             for(int i = 0; i < 4; i++){
                 for(int j = 0; j < 4; j++){
